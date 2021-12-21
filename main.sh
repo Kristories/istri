9e46a58700732c88e910c43f80db01f6eb6e6e2d
Wahyu Kristianto <w.kristories@gmail.com>
2021-12-22 02:56:34 +0700

    😱

---
+++ main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function ProgressBar {
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+    printf "\rMencari : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+}
+
+_start=1
+_end=100
+
+for number in $(seq ${_start} ${_end})
+do
+    sleep 0.1
+    ProgressBar ${number} ${_end}
+done
+
+printf '\n---------------------------------------------------------\n'
+openssl rsautl -inkey key.txt -decrypt < output.bin
+
